93e8f8d1153c7f3a82c25d68c66ccfab9428148e
Serhii151 <122047001+Serhii151@users.noreply.github.com>
2023-07-28 00:34:57 +0300

    hw up

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Form, Label, Input, Button } from './ContactForm.styled';
+import PropTypes from 'prop-types';
+
+const ContactForm = ({ name, number, handleChange, handleSubmit }) => {
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Label>
+        Name
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleChange}
+        />
+      </Label>
+      <Label>
+        Number
+        <Input
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleChange}
+        />
+      </Label>
+      <Button type="submit">Add Contact</Button>
+    </Form>
+  );
+};
+
+ContactForm.propTypes = {
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
